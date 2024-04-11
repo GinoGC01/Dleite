5d32fef69542a5217b97e6acb3718038d07b97e3
GinoDesktop <ginociancia10@gmail.com>
2024-04-11 12:20:29 -0300

    set card-product / products section removed

--- src/components/CardYogurtType.jsx
+++ src/components/CardYogurtType.jsx
@@ -2,7 +2,14 @@ import React, { useState } from "react";
 import "animate.css";
 import "../styles/CardType.css";
 
-export function CardYogurtType({ title, descripcion }) {
+export function CardYogurtType({
+  flag,
+  title,
+  descripcion,
+  cantidad,
+  adicionales,
+  stock,
+}) {
   const [open, setOpen] = useState(false);
 
   const handleOpenCard = () => {
@@ -10,7 +17,7 @@ export function CardYogurtType({ title, descripcion }) {
   };
   return (
     <>
-      <div className="cardtype-container" onClick={handleOpenCard}>
+      <li className="cardtype-container" onClick={handleOpenCard}>
         <strong className="title-card">{title}</strong>
         <span
           className={
@@ -21,17 +28,41 @@ export function CardYogurtType({ title, descripcion }) {
         >
           expand_more
         </span>
-      </div>
+      </li>
 
       {open && (
-        <div className="animate__animated animate__bounceIn Card-description-container">
-          <h3>Yogurt {title}</h3>
-          <img src="../../../OIP.jpeg" alt={title} />
+        <li className="animate__animated animate__bounceIn Card-description-container">
+          <div className="title">
+            <h3>Yogurt {title}</h3>
+            {flag != "" && <img src={flag} alt={title} className="flag" />}
+          </div>
+          <img src="../../../OIP.jpeg" alt={title} className="yogurt-image" />
+          <span className={stock ? "stock" : "no-stock"}>
+            {stock ? "Disponible" : "No disponible"}
+          </span>
           <div className="Card-text-button-container">
             <p>{descripcion}</p>
-            <button>Lo quiero!</button>
+            <div className="details">
+              <ul>
+                <h3>Cantidades:</h3>
+                {cantidad.medida.map((medida, index) => {
+                  return (
+                    <li key={index}>
+                      {medida} {cantidad.unidad}
+                    </li>
+                  );
+                })}
+              </ul>
+              <ul>
+                <h3>Adicionales:</h3>
+                {adicionales.map((adicional, index) => {
+                  return <li key={index}>{adicional}</li>;
+                })}
+              </ul>
+            </div>
+            <a href="#">Lo quiero!</a>
           </div>
-        </div>
+        </li>
       )}
     </>
   );
